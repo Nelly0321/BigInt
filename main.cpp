5b48db6b566dfc5244d203e95185c455d6538903
Nelly0321 <122158691+Nelly0321@users.noreply.github.com>
2023-02-11 14:26:22 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main(){
     ACA::BigInt a, b, c(876487);
     cin >> a;
     cin >> b;
-    /*cout << a+b << " a+b" << endl;
+    cout << a+b << " a+b" << endl;
     cout << a*b <<" a*b " << endl;
-    cout << a-b << endl;*/
+    cout << a-b << endl;
     cout << (a^b) << " a^b"<< endl;
-    //cout << boolalpha << (a>=b) << " : a>=b" << endl;
-    //cout << boolalpha << (a<=b) << " : a<=b" << endl;
+    cout << boolalpha << (a>=b) << " : a>=b" << endl;
+    cout << boolalpha << (a<=b) << " : a<=b" << endl;
     return 0;
 
 }
